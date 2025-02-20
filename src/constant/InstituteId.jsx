bd4ce6ea53ee9a07cab3bc2fb0fa55ecd92bf14b
hemlata6 <hemurajpoot6@gmail.com>
2025-02-20 19:02:37 +0530

    inst id changed

--- src/constant/InstituteId.jsx
+++ src/constant/InstituteId.jsx
@@ -3,12 +3,12 @@
 // const instId = 49;
 
 // providya
-export const auth = "eyJ1c2VySWQiOjEyMzc3LCJ0aW1lc3RhbXAiOjE3Mzk4NzE0MDg5NDksImV4cGlyeSI6MTc2OTg3MTQwODk0OX0=";
-const instId = 152;
+// export const auth = "eyJ1c2VySWQiOjEyMzc3LCJ0aW1lc3RhbXAiOjE3Mzk4NzE0MDg5NDksImV4cGlyeSI6MTc2OTg3MTQwODk0OX0=";
+// const instId = 152;
 
 //lecture dekho 
-// export const auth = "eyJ1c2VySWQiOjEyMzc3LCJ0aW1lc3RhbXAiOjE3Mzk4NzE0MDg5NDksImV4cGlyeSI6MTc2OTg3MTQwODk0OX0=";
-// const instId = 156;
+export const auth = "eyJ1c2VySWQiOjEyMzc3LCJ0aW1lc3RhbXAiOjE3Mzk4NzE0MDg5NDksImV4cGlyeSI6MTc2OTg3MTQwODk0OX0=";
+const instId = 156;
 
 
 export default instId;
